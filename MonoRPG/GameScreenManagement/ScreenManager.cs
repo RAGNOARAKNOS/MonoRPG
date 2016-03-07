abaef8648e334107a99b2874ca4d567378f03ee2
RAGNOARAKNOS <ragnocorp@googlemail.com>
2016-03-07 22:57:58 +0000

    MONO-1 Created ScreenManager DLL

---
+++ MonoRPG/GameScreenManagement/ScreenManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameScreenManagement
+{
+    public class ScreenManager : DrawableGameComponent
+    {
+        #region FIELDS
+        #endregion
+        #region PROPERTIES
+
+        #endregion
+
+        public ScreenManager(Game game)
+            : base(game)
+        {
+
+        }
+    }
+}
